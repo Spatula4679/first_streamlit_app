82adc2a6a63a7ef1a61fcce61fa1352b562f1887
Spatula4679 <140055819+Spatula4679@users.noreply.github.com>
2023-07-21 19:17:51 +0530

    Update streamlit_app.py
    tidyin up

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 
 import streamlit
+import pandas
+import snowflake.connector
+import requests
+from urllib.error import URLError
+
 streamlit.title("My Mom's New Healthy Diner")
 streamlit.header("Breakfast Favourites")
 streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
@@ -9,7 +14,8 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+#import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -22,13 +28,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #new section indicates fruityvice api response
-import requests
+
 
 streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) #separating the base url and the fruit name.
 # Normalises the JSON data received
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -38,7 +44,7 @@ streamlit.dataframe(fruityvice_normalized)
 #stop()
 streamlit.stop()
 
-import snowflake.connector
+#import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
